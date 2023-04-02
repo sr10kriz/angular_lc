ab6468e3e466d13c41080a488f890e726138b46f
Sridhar <sridhar.krishnan1202@gmail.com>
2023-04-02 19:03:58 +0530

    understanding decorator in component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({ // its a decorator function accepts an argument object this decorator function converts plain ts to angular component
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_lc';
+  title:string = 'angular_lc';
 }
